8bd544b102c28e37ca98f9b78f7f2a03464d20a5
Cominoda <leo.cominoda@proton.me>
2026-05-13 13:28:05 +0200

    zerty

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -1,4 +1,3 @@
-
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour
@@ -11,31 +10,65 @@ public class Obstacle : MonoBehaviour
 
     [SerializeField]
     private int Damages;
-    private float _startZ;
-    private bool isStrange  = false;
 
+    [SerializeField]
+    private float Amplitude;
+
+    [SerializeField]
+    private float Frequence;
+
+    private float ix = 0;
+    private float iy = 0;
+    //transform.position.z
+    private float _startZ;
+    private bool _isXSpecial = false;
+    private bool _isYSpecial = false;
+
+    [SerializeField]
+    private GameObject ExplosionParticles;
     void Start()
     {
         _startZ = transform.position.z;
-        //Random rnd = new Random();
-        //isStrange = Random.n.next(0, 10) == 1;
+        _isXSpecial = Random.Range(0, 10) == 1;
+        _isYSpecial = Random.Range(0, 10) == 1;
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        float x = Mathf.Sin(Time.deltaTime);
-        float y = Mathf.Cos(Time.deltaTime);
-        transform.position += new Vector3(x, y, -Speed * Time.deltaTime);
+        float x = 0;
+        float y = 0;
+        float z = -Speed * Time.deltaTime;
+        
+        if (_isXSpecial)
+        {
+            ix++;
+            x = Mathf.Sin(ix * Frequence) * Amplitude;
+            Debug.Log("X Special : " + x);
+        }
+        if (_isYSpecial)
+        {
+            iy++;
+            y = Mathf.Cos(iy * Frequence) * Amplitude;
+            Debug.Log("Y Special : " + y);
+        }
+        transform.position += new Vector3(x, y, z);
 
-        if(Mathf.Abs(transform.position.z - _startZ) < DestroyDistance)
+        if (Mathf.Abs(transform.position.z - _startZ) > DestroyDistance)
         {
             Destroy(gameObject);
         }
     }
+
     public int Explode()
     {
+        if(ExplosionParticles != null) {
+            Instantiate(ExplosionParticles, transform.position, Quaternion.identity);
+        }
+
         Destroy(gameObject);
         return Damages;
     }
 }
+
